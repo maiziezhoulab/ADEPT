0673aa4d79a5852a945c3431965af9eddd9a7407
Yunfei <yunfei.hu@vanderbilt.edu>
2023-03-06 15:50:16 -0600

    update param

--- GAAE/Train_ADEPT_modified.py
+++ GAAE/Train_ADEPT_modified.py
@@ -114,7 +114,7 @@ def train_STA_mod(adata, hidden_dims=None, n_epochs=750, lr=0.001,
 
     data = Transfer_pytorch_Data(adata_Vars)
 
-    model = STAGATE_mod2(hidden_dims=[adata_Vars.X.shape[1]]+hidden_dims, mlp_dims=7).to(device)
+    model = GAAE_mod2(hidden_dims=[adata_Vars.X.shape[1]]+hidden_dims, mlp_dims=7).to(device)
     # model = STAGATE_mod1(hidden_dims=hidden_dims, mlp_dims=7).to(device)
     data = data.to(device)
     print(model)
@@ -444,187 +444,187 @@ def DE_nzr(adata, hidden_dims=None, n_epochs=1000, lr=0.001, num_cluster=7,
     return nzr_
 
 
-def train_ADE_GO(adata, hidden_dims=None, n_epochs=1000, lr=0.001, num_cluster=4,
-                 gradient_clipping=5., weight_decay=0.0001, verbose=True, dif_k=200,
-                 random_seed=0, device=torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')):
-    """\
-    Training graph attention auto-encoder.
+# def train_ADE_GO(adata, hidden_dims=None, n_epochs=1000, lr=0.001, num_cluster=4,
+#                  gradient_clipping=5., weight_decay=0.0001, verbose=True, dif_k=200,
+#                  random_seed=0, device=torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')):
+#     """\
+#     Training graph attention auto-encoder.
 
-    Parameters
-    ----------
-    adata
-        AnnData object of scanpy package.
-    hidden_dims
-        The dimension of the encoder.
-    n_epochs
-        Number of total epochs in training.
-    lr
-        Learning rate for AdamOptimizer.
-    key_added
-        The latent embeddings are saved in adata.obsm[key_added].
-    gradient_clipping
-        Gradient Clipping.
-    weight_decay
-        Weight decay for AdamOptimizer.
-    save_loss
-        If True, the training loss is saved in adata.uns['STAGATE_loss'].
-    save_reconstrction
-        If True, the reconstructed expression profiles are saved in adata.layers['STAGATE_ReX'].
-    device
-        See torch.device.
+#     Parameters
+#     ----------
+#     adata
+#         AnnData object of scanpy package.
+#     hidden_dims
+#         The dimension of the encoder.
+#     n_epochs
+#         Number of total epochs in training.
+#     lr
+#         Learning rate for AdamOptimizer.
+#     key_added
+#         The latent embeddings are saved in adata.obsm[key_added].
+#     gradient_clipping
+#         Gradient Clipping.
+#     weight_decay
+#         Weight decay for AdamOptimizer.
+#     save_loss
+#         If True, the training loss is saved in adata.uns['STAGATE_loss'].
+#     save_reconstrction
+#         If True, the reconstructed expression profiles are saved in adata.layers['STAGATE_ReX'].
+#     device
+#         See torch.device.
 
-    Returns
-    -------
-    AnnData
-    """
+#     Returns
+#     -------
+#     AnnData
+#     """
 
-    # seed_everything()
-    if hidden_dims is None:
-        hidden_dims = [512, 30]
-    seed = random_seed
-    import random
-    random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-    np.random.seed(seed)
+#     # seed_everything()
+#     if hidden_dims is None:
+#         hidden_dims = [512, 30]
+#     seed = random_seed
+#     import random
+#     random.seed(seed)
+#     torch.manual_seed(seed)
+#     torch.cuda.manual_seed_all(seed)
+#     np.random.seed(seed)
 
-    adata.X = sp.csr_matrix(adata.X)
+#     adata.X = sp.csr_matrix(adata.X)
 
-    if 'highly_variable' in adata.var.columns:
-        adata_Vars = adata[:, adata.var['highly_variable']]
-    else:
-        adata_Vars = adata
+#     if 'highly_variable' in adata.var.columns:
+#         adata_Vars = adata[:, adata.var['highly_variable']]
+#     else:
+#         adata_Vars = adata
 
-    if verbose:
-        print('Size of Input: ', adata_Vars.shape)
-    if 'Spatial_Net' not in adata.uns.keys():
-        raise ValueError("Spatial_Net is not existed! Run Cal_Spatial_Net first!")
+#     if verbose:
+#         print('Size of Input: ', adata_Vars.shape)
+#     if 'Spatial_Net' not in adata.uns.keys():
+#         raise ValueError("Spatial_Net is not existed! Run Cal_Spatial_Net first!")
 
-    data = Transfer_pytorch_Data(adata_Vars)
+#     data = Transfer_pytorch_Data(adata_Vars)
 
-    model = STAGATE(hidden_dims=[adata_Vars.X.shape[1]]+hidden_dims).to(device)
-    # model = STAGATE_mod1(hidden_dims=hidden_dims, mlp_dims=7).to(device)
-    data = data.to(device)
-    print(model)
+#     model = GAAE(hidden_dims=[adata_Vars.X.shape[1]]+hidden_dims).to(device)
+#     # model = STAGATE_mod1(hidden_dims=hidden_dims, mlp_dims=7).to(device)
+#     data = data.to(device)
+#     print(model)
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
+#     optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
 
-    loss_list = []
-    for epoch in tqdm(range(1, n_epochs + 1)):
-        model.train()
-        optimizer.zero_grad()
-        embed, recon = model(data.x, data.edge_index)
-        loss = F.mse_loss(data.x, recon)  # F.nll_loss(out[data.train_mask], data.y[data.train_mask])
-        loss_list.append(loss.to('cpu').detach())
-        loss.backward()
-        torch.nn.utils.clip_grad_norm_(model.parameters(), gradient_clipping)
-        optimizer.step()
+#     loss_list = []
+#     for epoch in tqdm(range(1, n_epochs + 1)):
+#         model.train()
+#         optimizer.zero_grad()
+#         embed, recon = model(data.x, data.edge_index)
+#         loss = F.mse_loss(data.x, recon)  # F.nll_loss(out[data.train_mask], data.y[data.train_mask])
+#         loss_list.append(loss.to('cpu').detach())
+#         loss.backward()
+#         torch.nn.utils.clip_grad_norm_(model.parameters(), gradient_clipping)
+#         optimizer.step()
 
-    model.eval()
-    embed, recon = model(data.x, data.edge_index)
-    temp = embed.to('cpu').detach().numpy()
-    adata.obsm['test'] = temp
-    adata = mclust_R(adata, num_cluster=num_cluster, used_obsm='test')
-    # out = adata.obs.dropna()
-    # print(out['mclust'], out['Ground Truth'])
-    # ARI_ini = adjusted_rand_score(out['mclust'], out['Ground Truth'])
-    # print("ARI = ", ARI_ini)
+#     model.eval()
+#     embed, recon = model(data.x, data.edge_index)
+#     temp = embed.to('cpu').detach().numpy()
+#     adata.obsm['test'] = temp
+#     adata = mclust_R(adata, num_cluster=num_cluster, used_obsm='test')
+#     # out = adata.obs.dropna()
+#     # print(out['mclust'], out['Ground Truth'])
+#     # ARI_ini = adjusted_rand_score(out['mclust'], out['Ground Truth'])
+#     # print("ARI = ", ARI_ini)
 
-    # get DE from labels
-    print("selecting DEs after first round of clustering ...")
-    dif_genes_by_cluster_num = []
-    for i_ in range(num_cluster):
-        # print(set(adata.obs['mclust'].to_list()))
-        # print(adata.obs['mclust'] == i_+1)
-        keep_bcs_ = adata.obs.loc[adata.obs['mclust'] == i_+1].index
+#     # get DE from labels
+#     print("selecting DEs after first round of clustering ...")
+#     dif_genes_by_cluster_num = []
+#     for i_ in range(num_cluster):
+#         # print(set(adata.obs['mclust'].to_list()))
+#         # print(adata.obs['mclust'] == i_+1)
+#         keep_bcs_ = adata.obs.loc[adata.obs['mclust'] == i_+1].index
 
-        keep_bcs_control = adata.obs.loc[adata.obs['mclust'] != i_+1].index
-        #     print(keep_bcs_)
-        adata_new = adata[keep_bcs_].copy()
-        test_array = adata_new.X.toarray().T
+#         keep_bcs_control = adata.obs.loc[adata.obs['mclust'] != i_+1].index
+#         #     print(keep_bcs_)
+#         adata_new = adata[keep_bcs_].copy()
+#         test_array = adata_new.X.toarray().T
 
-        adata_gt_new_control = adata[keep_bcs_control].copy()
-        ctrl_array = adata_gt_new_control.X.toarray().T
+#         adata_gt_new_control = adata[keep_bcs_control].copy()
+#         ctrl_array = adata_gt_new_control.X.toarray().T
 
-        gene_list = adata.var.index[:, np.newaxis]
-        print(test_array.shape, ctrl_array.shape)
-        # print(gene_list)
-        print("label = ", i_+1)
-        # if num_cluster == 7:
-        #     dif_k = 200
-        # if num_cluster > 10:
-        #     dif_k = 70
-        dif_genes = dif_gene_analysis(test_array, ctrl_array, gene_list, dif_k)
+#         gene_list = adata.var.index[:, np.newaxis]
+#         print(test_array.shape, ctrl_array.shape)
+#         # print(gene_list)
+#         print("label = ", i_+1)
+#         # if num_cluster == 7:
+#         #     dif_k = 200
+#         # if num_cluster > 10:
+#         #     dif_k = 70
+#         dif_genes = dif_gene_analysis(test_array, ctrl_array, gene_list, dif_k)
 
-        dif_genes_new = []
-        for e in dif_genes:
-            dif_genes_new.append(e[0])
-        dif_genes_by_cluster_num.append(dif_genes_new)
-    union_ = set()
-    for i in range(num_cluster):
-        union_ = union_.union(set(dif_genes_by_cluster_num[i]))
+#         dif_genes_new = []
+#         for e in dif_genes:
+#             dif_genes_new.append(e[0])
+#         dif_genes_by_cluster_num.append(dif_genes_new)
+#     union_ = set()
+#     for i in range(num_cluster):
+#         union_ = union_.union(set(dif_genes_by_cluster_num[i]))
 
-    print("train a new AE with the potential DEs ...")
-    adata_new = adata[:, list(union_)]
-    data2 = Transfer_pytorch_Data(adata_new)
+#     print("train a new AE with the potential DEs ...")
+#     adata_new = adata[:, list(union_)]
+#     data2 = Transfer_pytorch_Data(adata_new)
 
-    model2 = STAGATE(hidden_dims=[adata_new.X.shape[1]] + hidden_dims).to(device)
-    # model = STAGATE_mod1(hidden_dims=hidden_dims, mlp_dims=7).to(device)
-    data2 = data2.to(device)
-    print(model2)
+#     model2 = GAAE(hidden_dims=[adata_new.X.shape[1]] + hidden_dims).to(device)
+#     # model = GAAE_mod1(hidden_dims=hidden_dims, mlp_dims=7).to(device)
+#     data2 = data2.to(device)
+#     print(model2)
 
-    optimizer = torch.optim.Adam(model2.parameters(), lr=lr, weight_decay=weight_decay)
+#     optimizer = torch.optim.Adam(model2.parameters(), lr=lr, weight_decay=weight_decay)
 
-    loss_list = []
-    for epoch in tqdm(range(1, n_epochs + 1)):
-        model2.train()
-        optimizer.zero_grad()
-        embed, recon = model2(data2.x, data2.edge_index)
-        loss = F.mse_loss(data2.x, recon)  # F.nll_loss(out[data.train_mask], data.y[data.train_mask])
-        loss_list.append(loss)
-        loss.backward()
-        torch.nn.utils.clip_grad_norm_(model2.parameters(), gradient_clipping)
-        optimizer.step()
+#     loss_list = []
+#     for epoch in tqdm(range(1, n_epochs + 1)):
+#         model2.train()
+#         optimizer.zero_grad()
+#         embed, recon = model2(data2.x, data2.edge_index)
+#         loss = F.mse_loss(data2.x, recon)  # F.nll_loss(out[data.train_mask], data.y[data.train_mask])
+#         loss_list.append(loss)
+#         loss.backward()
+#         torch.nn.utils.clip_grad_norm_(model2.parameters(), gradient_clipping)
+#         optimizer.step()
 
-    model2.eval()
-    embed, recon = model2(data2.x, data2.edge_index)
+#     model2.eval()
+#     embed, recon = model2(data2.x, data2.edge_index)
 
-    temp = embed.to('cpu').detach().numpy()
-    adata.obsm['test'] = temp
-    adata = mclust_R(adata, num_cluster=num_cluster, used_obsm='test')
-    # out = adata.obs.dropna()
+#     temp = embed.to('cpu').detach().numpy()
+#     adata.obsm['test'] = temp
+#     adata = mclust_R(adata, num_cluster=num_cluster, used_obsm='test')
+#     # out = adata.obs.dropna()
 
-    # get DE from labels
-    print("selecting DEs after first round of clustering ...")
-    dif_genes_by_cluster_num2 = []
-    for i_ in range(num_cluster):
-        # print(set(adata.obs['mclust'].to_list()))
-        # print(adata.obs['mclust'] == i_+1)
-        keep_bcs_ = adata.obs.loc[adata.obs['mclust'] == i_ + 1].index
+#     # get DE from labels
+#     print("selecting DEs after first round of clustering ...")
+#     dif_genes_by_cluster_num2 = []
+#     for i_ in range(num_cluster):
+#         # print(set(adata.obs['mclust'].to_list()))
+#         # print(adata.obs['mclust'] == i_+1)
+#         keep_bcs_ = adata.obs.loc[adata.obs['mclust'] == i_ + 1].index
 
-        keep_bcs_control = adata.obs.loc[adata.obs['mclust'] != i_ + 1].index
-        #     print(keep_bcs_)
-        adata_new = adata[keep_bcs_].copy()
-        test_array = adata_new.X.toarray().T
+#         keep_bcs_control = adata.obs.loc[adata.obs['mclust'] != i_ + 1].index
+#         #     print(keep_bcs_)
+#         adata_new = adata[keep_bcs_].copy()
+#         test_array = adata_new.X.toarray().T
 
-        adata_gt_new_control = adata[keep_bcs_control].copy()
-        ctrl_array = adata_gt_new_control.X.toarray().T
+#         adata_gt_new_control = adata[keep_bcs_control].copy()
+#         ctrl_array = adata_gt_new_control.X.toarray().T
 
-        gene_list = adata.var.index[:, np.newaxis]
-        print(test_array.shape, ctrl_array.shape)
-        # print(gene_list)
-        print("label = ", i_ + 1)
-        # if num_cluster == 7:
-        #     dif_k = 200
-        # if num_cluster > 10:
-        #     dif_k = 70
-        dif_genes = dif_gene_analysis(test_array, ctrl_array, gene_list, dif_k)
+#         gene_list = adata.var.index[:, np.newaxis]
+#         print(test_array.shape, ctrl_array.shape)
+#         # print(gene_list)
+#         print("label = ", i_ + 1)
+#         # if num_cluster == 7:
+#         #     dif_k = 200
+#         # if num_cluster > 10:
+#         #     dif_k = 70
+#         dif_genes = dif_gene_analysis(test_array, ctrl_array, gene_list, dif_k)
 
-        dif_genes_new = []
-        for e in dif_genes:
-            dif_genes_new.append(e[0])
-        # print(dif_genes_new)
-        dif_genes_by_cluster_num2.append(dif_genes_new)
-        # print(dif_genes_by_cluster_num2)
+#         dif_genes_new = []
+#         for e in dif_genes:
+#             dif_genes_new.append(e[0])
+#         # print(dif_genes_new)
+#         dif_genes_by_cluster_num2.append(dif_genes_new)
+#         # print(dif_genes_by_cluster_num2)
 
-    return dif_genes_by_cluster_num2
+#     return dif_genes_by_cluster_num2
